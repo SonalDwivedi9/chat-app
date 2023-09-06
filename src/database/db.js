6c8636fd032488e767685cb0e775da5f15c59f51
SonalDwivedi9 <119433405+SonalDwivedi9@users.noreply.github.com>
2023-09-06 23:10:28 +0530

    first commit

---
+++ src/database/db.js
@@ -0,0 +1,241 @@
+const file = {
+ 
+  friends: [
+    {
+      id: 2,
+      name: "Bhagat Singh",
+      // picture: "Images/img3.jpg",
+      picture: "https://i.pinimg.com/564x/29/6f/2a/296f2ac401460d41c5f0d5b201dd9db5.jpg",
+      chatlog: [
+        {
+          text: "Hey, how's it going?",
+          timestamp: "10:00",
+          side: "left",
+          message_id: 1,
+        },
+        {
+          text: "Not bad, still fighting for our nation's freedom. You?",
+          timestamp: "10:02",
+          side: "left",
+          message_id: 2,
+        },
+        {
+          text: "Same here, never giving up. We'll win this together!",
+          timestamp: "10:03",
+          side: "left",
+          message_id: 3,
+        },
+        {
+          text: "Absolutely! Our cause is worth it. Stay strong!",
+          timestamp: "10:03",
+          side: "left",
+          message_id: 4,
+        },
+        {
+          text: " You too, my friend. Freedom is our goal! ",
+          timestamp: "10:05",
+          side: "left",
+          message_id: 5,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Subhaash Chandra Bose",
+      picture: "https://i.pinimg.com/564x/9a/d5/3d/9ad53d61d329393d4d64ca836c647ffd.jpg",
+      chatlog: [
+        {
+          text: "Hey Subhas Chandra Bose, just wanted to say your efforts for India's independence were incredible!",
+          timestamp: "06:55",
+          side: "left",
+          message_id: 1,
+        },
+        {
+          text: " Thanks! It was a team effort by many dedicated individuals.",
+          timestamp: "06:58",
+          side: "left",
+          message_id: 2,
+        },
+        {
+          text: "Your leadership and determination were exceptional. What's your secret?",
+          timestamp: "07:00",
+          side: "left",
+          message_id: 3,
+        },
+        {
+          text: "Love for our nation and unwavering commitment to freedom. That's the key!",
+          timestamp: "07:05",
+          side: "left",
+          message_id: 4,
+        },
+        {
+          text: "Truly inspiring, Netaji.",
+          timestamp: "07:05",
+          side: "left",
+          message_id: 5,
+        },
+        {
+          text: "Thank you! Keep the flame of patriotism alive! ",
+          timestamp: "07:05",
+          side: "left",
+          message_id: 5,
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: "Chandra shekhar Azad",
+      picture: "https://e1.pxfuel.com/desktop-wallpaper/568/647/desktop-wallpaper-chandra-shekhar-azad-png-transparent-chandra-shekhar-azad.jpg",
+      chatlog: [
+        {
+          text: " Hey, how's it going?",
+          timestamp: "07:05",
+        },
+        {
+          text: " I'm good, thanks. Your commitment to India's freedom is inspiring.",
+          timestamp: "11:52",
+        },
+        {
+          text: "Thank you. It's our duty to fight for independence.",
+          timestamp: "11:52",
+        },
+      ],
+    },
+    {
+      id: 6,
+      name: "Rani Laxmi Bai",
+      picture: "https://i.pinimg.com/564x/23/c1/46/23c146f1a587358e355c0e793fe0295f.jpg",
+      chatlog: [
+        {
+          text: "Hello, your bravery is legendary!",
+          timestamp: "04:25",
+        },
+        {
+          text: " Thank you, my dear friend.",
+          timestamp: "05:55",
+        },
+      ],
+    },
+    {
+      id: 7,
+      name: "Dr. Bhim Rao Ambedkar",
+      picture: "https://st.adda247.com/https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2022/04/14070057/ambedkar-jayanti-history.jpg",
+      chatlog: [
+        {
+          text: "Hello, your work for social justice is remarkable!",
+          timestamp: "09:05",
+        },
+        {
+          text: " Thank you, my friend. It was a lifelong commitment.",
+          timestamp: "10:06",
+        },
+      ],
+    },
+    {
+      id: 8,
+      name: "Lala lajpat Ray",
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnuaoolaRaI1O9Q3shuB9HezGMMWTqdcnWaNav-4qhAA&s",
+      chatlog: [],
+    },
+    {
+      id: 9,
+      name: "Sarojni Naidu",
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS08DwvEpmTEh9qDKIjXJNTzvHSw7cavINY-1QmxD6QMA&s",
+      chatlog: [],
+    },
+    {
+      id: 10,
+      name: "Sardar Vallabh Bhai Patel",
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVzzHGGaiH9RLDnyv9GKqWUBeK3Fu8IVVnF0WlaJlSow&s",
+      chatlog: [],
+    },
+    {
+      id: 11,
+      name: "C. Rajagopalachari",
+      picture: "https://bharatmatamandir.in/wp-content/uploads/2008/05/271-779x1024.jpg",
+      chatlog: [],
+    },
+    {
+      id: 12,
+      name: "V.D. Savarkar",
+      picture: "https://m.media-amazon.com/images/I/61U9z8hb7-L._AC_UF894,1000_QL80_.jpg",
+      chatlog: [],
+    },
+    {
+      id: 13,
+      name: "Annie Besant",
+      picture: "https://images.news18.com/ibnlive/uploads/2020/10/annie-besant.jpg",
+      chatlog: [],
+    },
+    {
+      id: 14,
+      name: "Abdul Kalam",
+      picture: "https://i.pinimg.com/1200x/36/28/66/362866d89fd4587d7419fa74845dedec.jpg",
+      chatlog: [],
+    },
+    {
+      id: 15,
+      name: "Mahatma Gandhi",
+      picture: "https://i.pinimg.com/originals/df/4e/ce/df4ece3c6133d85d25e85fdfa88cd4fa.png",
+      chatlog: [],
+    },
+  ],
+  allFriends: [
+    // {
+    //   "id": 16,
+    //   "name": "A'Koba",
+    //   "picture": "",
+    //   "status": "Available"
+    // },
+    {
+      id: 15,
+      name: "Mahatma Gandhi",
+      picture: "https://i.pinimg.com/originals/df/4e/ce/df4ece3c6133d85d25e85fdfa88cd4fa.png",
+      status: "Busy",
+    },
+    {
+      id: 14,
+      name: "Abdul Kalam",
+      picture: "https://i.pinimg.com/1200x/36/28/66/362866d89fd4587d7419fa74845dedec.jpg",
+      status: "Busy",
+    },
+    {
+      id: 13,
+      name: "Annie Besant",
+      picture: "https://images.news18.com/ibnlive/uploads/2020/10/annie-besant.jpg",
+      status: "Busy",
+    },
+    {
+      id: 12,
+      name: "V.D. Savarkar",
+      picture: "https://m.media-amazon.com/images/I/61U9z8hb7-L._AC_UF894,1000_QL80_.jpg",
+      status: "Busy",
+    },
+    {
+      id: 11,
+      name: "C. Rajagopalachari",
+      picture: "https://bharatmatamandir.in/wp-content/uploads/2008/05/271-779x1024.jpg",
+      status: "Busy",
+    },
+    {
+      id: 10,
+      name: "Sardar Vallabh Bhai Patel",
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVzzHGGaiH9RLDnyv9GKqWUBeK3Fu8IVVnF0WlaJlSow&s",
+      status: "Busy",
+    },
+    {
+      id: 9,
+      name: "Sarojni Naidu",
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS08DwvEpmTEh9qDKIjXJNTzvHSw7cavINY-1QmxD6QMA&s",
+      status: "Busy",
+    },
+    {
+      id: 8,
+      name: "Lala lajpat Ray",
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnuaoolaRaI1O9Q3shuB9HezGMMWTqdcnWaNav-4qhAA&s",
+      status: "Busy",
+    },
+  ],
+};
+
+export default file;
